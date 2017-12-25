d98f1ca2c2fe75d8dbd94940319b592aef7e26ca
Neel Raiyani <nraiyani@gmail.com>
2017-12-25 13:32:19 -0500

    page loading

--- js/script.js
+++ js/script.js
@@ -2,10 +2,23 @@
 	var thisParent = document.currentScript.parentElement;
 	var xhr = new XMLHttpRequest();
 	xhr.onload = function() {
-		debugger;
+		thisParent.setAttribute("id", name);
 		thisParent.innerHTML += this.responseText;
+
+		buildAside(thisParent);
 	}
 	xhr.open("GET", name);
 	xhr.responseType = "text";
 	xhr.send();
+}
+
+function buildAside(sectionElem) {
+	var h2 = sectionElem.querySelector("h2");
+	var asideUl = document.querySelector("aside>ul");
+	var li = document.createElement("li");
+	var a = document.createElement("a");
+	a.href = "#" + sectionElem.id;
+	a.innerText = h2.innerText;
+	li.appendChild(a);
+	asideUl.appendChild(li);
 }
