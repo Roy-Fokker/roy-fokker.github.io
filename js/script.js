07b018c9b0421c08e66d5f7595ba6676ce24dc81
Neel Raiyani <nraiyani@gmail.com>
2017-12-25 12:50:59 -0500

    more

---
+++ js/script.js
@@ -0,0 +1,11 @@
+﻿function getHtml(name) {
+	var thisParent = document.currentScript.parentElement;
+	var xhr = new XMLHttpRequest();
+	xhr.onload = function() {
+		debugger;
+		thisParent.innerHTML += this.responseText;
+	}
+	xhr.open("GET", name);
+	xhr.responseType = "text";
+	xhr.send();
+}
--- js/scripts.js
+++
@@ -1,5 +0,0 @@
-﻿/// <reference path="//code.jquery.com/jquery-2.1.4.min.js" />
-
-$(function () {
-	
-});
